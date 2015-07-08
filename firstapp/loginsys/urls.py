96724ef2ed1a8e431cc9a67cc9fa2d0bb5f0c3bd
yesterday69 <yesterday69@yandex.ru>
2015-07-08 21:13:17 +0300

    MSG

--- firstapp/loginsys/urls.py
+++ firstapp/loginsys/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, patterns
 
 urlpatterns = patterns('',
-                       url(r'^login/$', 'loginsys.views.login'),
-                       url(r'^logout/$', 'loginsys.views.logout'),
-                       url(r'^register/$', 'loginsys.views.register'),
+                       url(r'^login', 'loginsys.views.login'),
+                       url(r'^logout', 'loginsys.views.logout'),
+                       url(r'^register', 'loginsys.views.register'),
                        )
